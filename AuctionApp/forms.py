bc8b8a68f8daa13b5d0654a9811d3fa5cace6d85
Hem Raj Regmi <sangamsyabil@gmail.com>
2018-04-26 17:58:40 -0400

    Basic Setup done
    Models are done and working on forms.

---
+++ AuctionApp/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Product, Bidder, Seller
+
+
+class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=100, required=True)
+    last_name = forms.CharField(max_length=100, required=True)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'user_name', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(UserCreateForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+
+class AddProductForm(forms.ModelForm):
+    bid_end_date = forms.DateField(widget=forms.TextInput(attrs={'class': 'datepicker'}))
+
+    class Meta:
+        model = Product
+        fields = ('product_name', 'image', 'category', 'description', 'selling_price', 'bid_end_date')
+
---
+++ AuctionApp/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import User
+from time import time
+from django.core.validators import RegexValidator
+
+
+# Create your models here.
+
+
+def getImage(instance, filename):
+    return "auction_system/image_{0}_{1}".format(str(time()), filename)
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to=getImage)
+    category = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    minimum_price = models.IntegerField(null=True)
+    selling_price = models.IntegerField(null=False)
+    bid_end_date = models.DateTimeField(default=None)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.id
+
+
+class Seller(models.Model):
+    user_name = models.ForeignKey(User)
+    product_id = models.ForeignKey(Product)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return str(self.user_name)
+
+
+class Bidder(models.Model):
+    user_name = models.ForeignKey(User)
+    product_id = models.ForeignKey(Product)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    numeric = RegexValidator(r'^[0-9]*$', 'Only numerics are allowed.')
+    bid_amount = models.CharField(max_length=100, validators=[numeric])
+
+    def __unicode__(self):
+        return str(self.user_name)
+
